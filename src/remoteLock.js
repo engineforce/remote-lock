0c28c8336b583636f9f300a7fb530554610d5dac
Paul Li <paul.li@caradvice.com.au>
2019-04-04 10:42:19 +1100

    Cleanup interface

--- src/remoteLock.js
+++ src/remoteLock.js
@@ -3,56 +3,58 @@
  * @param {(input: IGetLockInput) => Promise<string>} input.getLock
  * @param {(input: ISetLockInput) => Promise<void>} input.setLock
  * @param {(input: IReleaseLockInput) => Promise<void>} input.releaseLock
- * @param {number} input.pollingTimeout
- * @param {number} input.totalTimeout
+ * @param {number=} input.pollingTimeout
+ * @param {number=} input.totalTimeout
  */
 export const remoteLock = ({
-  getLock,
-  setLock,
-  releaseLock,
-  pollingTimeout,
-  totalTimeout
+  getLock: _getLock,
+  setLock: _setLock,
+  releaseLock: _releaseLock,
+  pollingTimeout: _pollingTimeout,
+  totalTimeout: _totalTimeout,
 }) => {
-  pollingTimeout = pollingTimeout || 1000;
-  totalTimeout = totalTimeout || 30000;
-
   return (
     /**
      * @param {object} input
      * @param {string} input.lockId
      * @param {(input: IExecInput) => Promise<any>} input.exec
-     * @param {() => Promise<boolean>} input.skipLock
+     * @param {() => Promise<boolean>=} input.skipLock
+     * @param {number=} input.pollingTimeout
+     * @param {number=} input.totalTimeout
      */
-    async ({ lockId, exec, skipLock }) => {
-      console.assert(lockId != undefined, 'Lock ID is empty.');
+    async ({ lockId, exec, skipLock, pollingTimeout, totalTimeout }) => {
+      console.assert(lockId != undefined, 'Lock ID is empty.')
 
-      let hasLock = false;
+      pollingTimeout = pollingTimeout || _pollingTimeout || 1000
+      totalTimeout = totalTimeout || _totalTimeout || 60000
+
+      let hasLock = false
 
       try {
         hasLock = await pollForLock({
-          getLock,
-          setLock,
+          getLock: _getLock,
+          setLock: _setLock,
           lockId,
           pollingTimeout,
           skipLock,
-          totalTimeout
-        });
+          totalTimeout,
+        })
 
-        return exec({ hasLock });
+        return exec({ hasLock })
       } finally {
         if (hasLock) {
-          await releaseLock({ lockId });
+          await _releaseLock({ lockId })
         }
       }
     }
-  );
-};
+  )
+}
 
 /**
  * @param {object} input
  * @param {(input: IGetLockInput) => Promise<string>} input.getLock
  * @param {(input: ISetLockInput) => Promise<void>} input.setLock
- * @param {() => Promise<boolean>} input.skipLock
+ * @param {() => Promise<boolean>=} input.skipLock
  * @param {string} input.lockId
  * @param {number} input.pollingTimeout
  * @param {number} input.totalTimeout
@@ -63,41 +65,41 @@ async function pollForLock({
   skipLock,
   lockId,
   getLock,
-  setLock
+  setLock,
 }) {
-  let count = 0;
-  let hasLock = false;
-  const startTime = new Date().getTime();
+  let count = 0
+  let hasLock = false
+  const startTime = new Date().getTime()
 
   while (true) {
-    count++;
+    count++
     if (typeof skipLock === 'function' && count > 1) {
       if (await skipLock()) {
-        break;
+        break
       }
     }
 
-    const currentTime = new Date().getTime();
+    const currentTime = new Date().getTime()
     if (currentTime - startTime > totalTimeout) {
-      throw new Error(`Failed to obtain lock after ${totalTimeout} ms.`);
+      throw new Error(`Failed to obtain lock after ${totalTimeout} ms.`)
     }
 
-    const currentLockId = await getLock({ lockId });
+    const currentLockId = await getLock({ lockId })
 
     if (currentLockId == undefined) {
-      await setLock({ lockId, timeout: totalTimeout });
-      continue;
+      await setLock({ lockId, timeout: totalTimeout })
+      continue
     }
 
     if (currentLockId == lockId) {
-      hasLock = true;
-      break;
+      hasLock = true
+      break
     }
 
-    await new Promise(resolve => setTimeout(resolve, pollingTimeout));
+    await new Promise(resolve => setTimeout(resolve, pollingTimeout))
   }
 
-  return hasLock;
+  return hasLock
 }
 
 /**
