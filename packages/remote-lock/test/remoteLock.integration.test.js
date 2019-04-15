28884c53bd90559e6e5cb7fcf32b611cc0655761
Paul Li <paul.li@caradvice.com.au>
2019-04-15 23:52:55 +1000

    Fixed remoteLock.integration.test.js

--- packages/remote-lock/test/remoteLock.integration.test.js
+++ packages/remote-lock/test/remoteLock.integration.test.js
@@ -1,10 +1,35 @@
 import { makeRemoteLock } from '../src/remoteLock'
+import { equals } from 'ramda'
 
 describe('given remoteLock', () => {
   describe('when calling remoteLock once with no existing lock', () => {
     it('should call getLock only once', async () => {
-      await exec()
-    })
+      let expectedRequestIds = [
+        ['1', '2', '3'],
+        ['1', '3', '2'],
+        ['2', '1', '3'],
+        ['2', '3', '1'],
+        ['3', '1', '2'],
+        ['3', '2', '1'],
+      ]
+      while (expectedRequestIds.length > 0) {
+        const { requests, executedRequestIds } = await exec()
+
+        expect(requests[0].exec).toHaveBeenCalledTimes(1)
+        expect(requests[1].exec).toHaveBeenCalledTimes(1)
+        expect(requests[2].exec).toHaveBeenCalledTimes(1)
+
+        expect(requests[0].exec.mock.calls[0][0]).toEqual({ hasLock: true })
+        expect(requests[1].exec.mock.calls[0][0]).toEqual({ hasLock: true })
+        expect(requests[2].exec.mock.calls[0][0]).toEqual({ hasLock: true })
+
+        expectedRequestIds = expectedRequestIds.filter(
+          (requestIds) => !equals(requestIds, executedRequestIds)
+        )
+
+        // await new Promise((resolve) => setTimeout(resolve, 1000))
+      }
+    }, 600000)
 
     async function exec() {
       const requests = [
@@ -24,15 +49,28 @@ describe('given remoteLock', () => {
 
       const { remoteLock } = getTestContext()
 
-      await new Promise((resolve, reject) => {
-        Promise.all(requests.map((request) => remoteLock(request))).then(
-          resolve,
-          reject
-        )
+      /**
+       * @type {string[]}
+       */
+      const executedRequestIds = []
+
+      await new Promise((resolve1, reject1) => {
+        Promise.all(
+          requests.map((request) => {
+            return new Promise((resolve2) => {
+              setTimeout(async () => {
+                executedRequestIds.push(request.requestId)
+                await remoteLock(request)
+                resolve2()
+              }, Math.ceil(Math.random() * 1000))
+            })
+          })
+        ).then(resolve1, reject1)
       })
 
       return {
         requests,
+        executedRequestIds,
       }
     }
   })
