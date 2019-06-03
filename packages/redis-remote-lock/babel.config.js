bd00be31ea76cf9539f58fefbaa8f8ef7393448a
Paul Li <paul.li@caradvice.com.au>
2019-06-03 10:20:44 +1000

    Added missing files

--- packages/redis-remote-lock/babel.config.js
+++ packages/redis-remote-lock/babel.config.js
@@ -10,14 +10,7 @@ module.exports = {
       },
     ],
   ],
-  plugins: [
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        corejs: 2,
-      },
-    ],
-  ],
+  plugins: [['@babel/plugin-transform-runtime']],
   env: {
     test: {
       presets: [
